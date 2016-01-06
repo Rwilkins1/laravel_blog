30fe699c75978d48e2e434c8f54c5050c4bcad95
Reagan Wilkins <reagan.wilkins@gmail.com>
2016-01-06 12:02:02 -0600

    completed the routing exercise

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,42 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+// Route::get('/resume', function() {
+// 	return "This is my resume";
+// });
+
+// Route::get('/portfolio', function() {
+// 	return "This is my portfolio";
+// });
+
+// Route::get('/{route?}', function($route = null) 
+// {
+// 	return "This is my $route";
+// });
+
+Route::get('/sayhello/{name}', function($name)
+{
+    if ($name == "Chris") {
+        return Redirect::to('/');
+    } else {
+        $data = array('name' => $name);
+        return View::make('my-first-view')->with($data);
+    }
+});
+
+Route::get('/rolldice/{guess?}', function($guess = null) {
+	$randomnumber = mt_rand(1, 6);
+	if($guess != null && $guess == $randomnumber) {
+		$result = "You guessed correctly!";
+		$guess = "You guessed $guess!";
+	} else if ($guess != null && $guess != $randomnumber) {
+		$result = "You guessed incorrectly!";
+		$guess = "You guessed $guess!";
+	} else if ($guess == null) {
+		$result = "";
+		$guess = '';
+	}
+	$data = array('guess' => $guess, 'randomnumber' => $randomnumber, 'result' => $result);
+	return View::make('roll-dice')->with($data);
+});
--- readme.md
+++ readme.md
@@ -2,5 +2,12 @@
 
 This is a blog that I wrote using PHP, JS, Jquery and CSS on the Laravel Framework.
 
+## Getting Started
+-Clone this repository
+-Create a database for this application
+-Create a '.env.local.php' file inside of 'blog.dev/'
+-Add the contents of "env-template.php' to '.env.local.php'
+-Add your database logins to the values
+-Run 'composer install'
 
 The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
